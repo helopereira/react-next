b9709bbdfe17d2603b6c5773a602b9facc54b24a
helopereira <helopereira689@gmail.com>
2023-12-27 15:36:23 -0300

    modified:   src/app/layout.tsx 	modified:   src/components/basicos/Caixa.tsx 	new file:   src/pages/_app.tsx 	new file:   src/pages/_documents.tsx 	modified:   src/pages/basicos/caixa.tsx 	renamed:    src/app/globals.css -> src/styles/globals.css

--- src/components/basicos/Caixa.tsx
+++ src/components/basicos/Caixa.tsx
@@ -1,11 +1,18 @@
-export default function Caixa(){
+
+
+export default function Caixa(props: any){
     return(
-        <div className="
-        bg-purlple-500
-        w-64
-        h-64
-        ">
-        Caixa
+        <div className={`
+            bg-pink-200
+            w-64
+            h-64
+            m-2
+            text-center
+            text-5xl
+            font-black
+            flex justify-center items-center
+            `}>
+        {props.children}
         </div>
     )  
 }
--- src/pages/basicos/caixa.tsx
+++ src/pages/basicos/caixa.tsx
@@ -2,8 +2,16 @@ import Caixa from "@/components/basicos/Caixa";
 
 export default function paginaCaixa(){
     return(
-        <div>
-            <Caixa/>
+        <div className={`
+        flex flex-row
+        align-middle
+        justify-items-center
+        g-7
+        `}
+        >
+            <Caixa>1</Caixa>
+            <Caixa>2</Caixa>
+            <Caixa>3</Caixa>
         </div>
     )
 }
